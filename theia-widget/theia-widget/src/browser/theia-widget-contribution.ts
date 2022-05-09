a963d9526a57e90477f46edf9e122e65f7cd7a79
Abhishek Chakraborty <acharkab@xilinx.com>
2022-05-09 11:30:23 +0100

    Add command to test statusbar onclick

--- theia-widget/theia-widget/src/browser/theia-widget-contribution.ts
+++ theia-widget/theia-widget/src/browser/theia-widget-contribution.ts
@@ -1,32 +1,51 @@
-import { injectable } from '@theia/core/shared/inversify';
-import { MenuModelRegistry } from '@theia/core';
-import { TheiaWidgetWidget } from './theia-widget-widget';
-import { AbstractViewContribution } from '@theia/core/lib/browser';
-import { Command, CommandRegistry } from '@theia/core/lib/common/command';
+import { inject, injectable } from "@theia/core/shared/inversify";
+import {
+  MenuModelRegistry,
+  MessageService,
+  ProgressService,
+} from "@theia/core";
+import { TheiaWidgetWidget } from "./theia-widget-widget";
+import {
+  AbstractViewContribution,
+  StatusBar,
+  StatusBarAlignment,
+} from "@theia/core/lib/browser";
+import { Command, CommandRegistry } from "@theia/core/lib/common/command";
 
-export const TheiaWidgetCommand: Command = { id: 'theia-widget:command' };
+export const TheiaWidgetCommand: Command = { id: "theia-widget:command" };
+
+export const TestProgressCommand: Command = {
+  id: "theia-widget:progress-status-command",
+  label: "Show running progress",
+};
 
 @injectable()
 export class TheiaWidgetContribution extends AbstractViewContribution<TheiaWidgetWidget> {
+  @inject(MessageService)
+  protected readonly messageService: MessageService;
 
-    /**
-     * `AbstractViewContribution` handles the creation and registering
-     *  of the widget including commands, menus, and keybindings.
-     * 
-     * We can pass `defaultWidgetOptions` which define widget properties such as 
-     * its location `area` (`main`, `left`, `right`, `bottom`), `mode`, and `ref`.
-     * 
-     */
-    constructor() {
-        super({
-            widgetId: TheiaWidgetWidget.ID,
-            widgetName: TheiaWidgetWidget.LABEL,
-            defaultWidgetOptions: { area: 'left' },
-            toggleCommandId: TheiaWidgetCommand.id
-        });
-    }
+  @inject(ProgressService) protected readonly progressService: ProgressService;
 
-    /**
+  @inject(StatusBar)
+  protected readonly statusBar: StatusBar;
+  /**
+   * `AbstractViewContribution` handles the creation and registering
+   *  of the widget including commands, menus, and keybindings.
+   *
+   * We can pass `defaultWidgetOptions` which define widget properties such as
+   * its location `area` (`main`, `left`, `right`, `bottom`), `mode`, and `ref`.
+   *
+   */
+  constructor() {
+    super({
+      widgetId: TheiaWidgetWidget.ID,
+      widgetName: TheiaWidgetWidget.LABEL,
+      defaultWidgetOptions: { area: "left" },
+      toggleCommandId: TheiaWidgetCommand.id,
+    });
+  }
+
+  /**
      * Example command registration to open the widget from the menu, and quick-open.
      * For a simpler use case, it is possible to simply call:
      ```ts
@@ -44,13 +63,57 @@ export class TheiaWidgetContribution extends AbstractViewContribution<TheiaWidge
      *
      * @param commands
      */
-    registerCommands(commands: CommandRegistry): void {
-        commands.registerCommand(TheiaWidgetCommand, {
-            execute: () => super.openView({ activate: false, reveal: true })
-        });
-    }
+  registerCommands(commands: CommandRegistry): void {
+    commands.registerCommand(TheiaWidgetCommand, {
+      execute: () => super.openView({ activate: false, reveal: true }),
+    });
 
-    /**
+    commands.registerCommand(TestProgressCommand, {
+      execute: async () => {
+        const id = "show-running-tasks";
+        this.statusBar.setElement(id, {
+          text: `testing : progress in status`,
+          tooltip: "testing : progress in status",
+          alignment: StatusBarAlignment.LEFT,
+          priority: 2,
+          onclick: () => this.messageService.info("Test status bar"),
+        });
+
+        // await this.progressService.showProgress({
+        //   text: `testing : progress in status`,
+        //   options: { location: "window" },
+        //   onClick: () => this.messageService.info("Test status bar"),
+        // });
+        // this.messageService
+        //   .showProgress({
+        //     text: "Starting to report progress",
+        //   })
+        //   .then((progress) => {
+        //     window.setTimeout(() => {
+        //       progress.report({
+        //         message: "First step completed",
+        //         work: { done: 25, total: 100 },
+        //       });
+        //     }, 2000);
+        //     window.setTimeout(() => {
+        //       progress.report({
+        //         message: "Next step completed",
+        //         work: { done: 60, total: 100 },
+        //       });
+        //     }, 4000);
+        //     window.setTimeout(() => {
+        //       progress.report({
+        //         message: "Complete",
+        //         work: { done: 100, total: 100 },
+        //       });
+        //     }, 6000);
+        //     window.setTimeout(() => progress.cancel(), 7000);
+        //   });
+      },
+    });
+  }
+
+  /**
      * Example menu registration to contribute a menu item used to open the widget.
      * Default location when extending the `AbstractViewContribution` is the `View` main-menu item.
      * 
@@ -64,7 +127,7 @@ export class TheiaWidgetContribution extends AbstractViewContribution<TheiaWidge
      * 
      * @param menus
      */
-    registerMenus(menus: MenuModelRegistry): void {
-        super.registerMenus(menus);
-    }
+  registerMenus(menus: MenuModelRegistry): void {
+    super.registerMenus(menus);
+  }
 }
--- theia-widget/theia-widget/src/browser/theia-widget-widget.tsx
+++ theia-widget/theia-widget/src/browser/theia-widget-widget.tsx
@@ -1,63 +1,72 @@
-import * as React from 'react';
-import { injectable, postConstruct, inject } from '@theia/core/shared/inversify';
-import { AlertMessage } from '@theia/core/lib/browser/widgets/alert-message';
-import { ReactWidget } from '@theia/core/lib/browser/widgets/react-widget';
-import { MessageService } from '@theia/core';
-import {WorkspaceService} from '@theia/workspace/lib/browser';
-import { ProblemManager } from '@theia/markers/lib/browser/problem/problem-manager';
-import URI from '@theia/core/lib/common/uri';
+import * as React from "react";
+import {
+  injectable,
+  postConstruct,
+  inject,
+} from "@theia/core/shared/inversify";
+import { AlertMessage } from "@theia/core/lib/browser/widgets/alert-message";
+import { ReactWidget } from "@theia/core/lib/browser/widgets/react-widget";
+import { MessageService } from "@theia/core";
+import { WorkspaceService } from "@theia/workspace/lib/browser";
+import { ProblemManager } from "@theia/markers/lib/browser/problem/problem-manager";
+import URI from "@theia/core/lib/common/uri";
 
 // import {EditorManager} from '@theia/editor/lib/browser';
 @injectable()
 export class TheiaWidgetWidget extends ReactWidget {
+  static readonly ID = "theia-widget:widget";
+  static readonly LABEL = "TheiaWidget Widget";
 
-    static readonly ID = 'theia-widget:widget';
-    static readonly LABEL = 'TheiaWidget Widget';
+  @inject(MessageService)
+  protected readonly messageService!: MessageService;
 
-    @inject(MessageService)
-    protected readonly messageService!: MessageService;
+  @inject(ProblemManager)
+  protected readonly problemManager: ProblemManager;
 
-    
-    @inject(ProblemManager)
-    protected readonly problemManager: ProblemManager;
+  @inject(WorkspaceService)
+  private readonly workspaceService: WorkspaceService;
 
-    @inject(WorkspaceService)
-    private readonly workspaceService: WorkspaceService;
+  @postConstruct()
+  protected async init(): Promise<void> {
+    this.id = TheiaWidgetWidget.ID;
+    this.title.label = TheiaWidgetWidget.LABEL;
+    this.title.caption = TheiaWidgetWidget.LABEL;
+    this.title.closable = true;
+    this.title.iconClass = "fa fa-window-maximize"; // example widget icon.
+    this.update();
+  }
 
-    // @inject(EditorManager)
-    // protected readonly editorManager: EditorManager;
-    
-    @postConstruct()
-    protected async init(): Promise < void> {
-        this.id = TheiaWidgetWidget.ID;
-        this.title.label = TheiaWidgetWidget.LABEL;
-        this.title.caption = TheiaWidgetWidget.LABEL;
-        this.title.closable = true;
-        this.title.iconClass = 'fa fa-window-maximize'; // example widget icon.
-        this.update();
-    }
-
-    render(): React.ReactElement {
-        const header = `This is a sample widget which simply calls the messageService
+  render(): React.ReactElement {
+    const header = `This is a sample widget which simply calls the messageService
         in order to display an info message to end users.`;
-        return <div id='widget-container'>
-            <AlertMessage type='INFO' header={header} />
-            <button className='theia-button secondary' title='Display Message' onClick={_a => this.displayMessage()}>Display Message</button>
-        </div>
-    }
+    return (
+      <div id="widget-container">
+        <AlertMessage type="INFO" header={header} />
+        <button
+          className="theia-button secondary"
+          title="Display Message"
+          onClick={(_a) => this.displayMessage()}
+        >
+          Display Message
+        </button>
+      </div>
+    );
+  }
 
-    protected displayMessage(): void {
-        this.messageService.info('Congratulations: TheiaWidget Widget Successfully Created!');
-        this.workspaceService.getWorkspaceRootUri(new URI('asdf'));
-    }
+  protected displayMessage(): void {
+    this.messageService.info(
+      "Congratulations: TheiaWidget Widget Successfully Created!"
+    );
+    this.workspaceService.getWorkspaceRootUri(new URI("asdf"));
+  }
 
-    // currentFileEditorWidget = (): EditorWidget | undefined => {
-    //     const editorWidget = this.editorManager.currentEditor;
-    //     if (editorWidget) {
-    //         if (editorWidget.editor.uri.scheme === 'file') {
-    //             return editorWidget;
-    //         }
-    //     }
-    //     return undefined;
-    // };
+  // currentFileEditorWidget = (): EditorWidget | undefined => {
+  //     const editorWidget = this.editorManager.currentEditor;
+  //     if (editorWidget) {
+  //         if (editorWidget.editor.uri.scheme === 'file') {
+  //             return editorWidget;
+  //         }
+  //     }
+  //     return undefined;
+  // };
 }
